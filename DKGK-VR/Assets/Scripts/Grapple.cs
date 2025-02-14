2eeab0ca9d27f0d8512f9038bd40d29a0683e8d5
octowild <139123949+octowild@users.noreply.github.com>
2025-02-14 14:59:55 +0530

    coding

--- DKGK-VR/Assets/Scripts/Grapple.cs
+++ DKGK-VR/Assets/Scripts/Grapple.cs
@@ -7,11 +7,17 @@ public class Grapple : MonoBehaviour
 {
     public GameObject GrappleGun;
     public GameObject GrappleHead;
+    public GameObject BarrelPos;
 
     public float _GrappleHeadSpeed = 10f;
 
     private Vector3 _GunDir;
+    private Vector3 _ReelDir;
     private bool _Shooting=false;
+    private bool _HeadCol=false;
+    private bool _Attached = false;
+    private bool _release=false;
+    private bool _Reeling=false;
 
     void Start()
     {
@@ -27,8 +33,16 @@ public class Grapple : MonoBehaviour
         {
             _GunDir = (GrappleGun.transform.up * -1);
         }
-        if (_Shooting) {
+        if (_Shooting && !_HeadCol)
+        {
             GrappleHead.transform.position += _GunDir * _GrappleHeadSpeed * Time.deltaTime;
+        }
+        else if (_Shooting && _HeadCol)
+        {
+            _Attached = true;
+        }
+        if (_Reeling) {
+            GrappleReel();
         }
     }
     public void GrappleShoot()
@@ -38,5 +52,31 @@ public class Grapple : MonoBehaviour
     public void GrappleStop()
     {
         _Shooting = false;
+        _release = true;
+        //GrappleHead.transform.position=BarrelPos.transform.position;
+    }
+    public void GrappleReel()
+    {
+        if (!_HeadCol)
+        {
+            if (_Attached||_release)
+            {
+                _ReelDir = BarrelPos.transform.position- GrappleHead.transform.position;
+                GrappleHead.transform.position += _ReelDir.normalized * _GrappleHeadSpeed * Time.deltaTime;
+            }
+            else
+            {
+                _ReelDir = GrappleHead.transform.position- BarrelPos.transform.position;
+                //move player here
+            }
+
+
+
+            if (_ReelDir.magnitude <= 5f)
+            {
+                GrappleHead.transform.position=BarrelPos.transform.position;
+                _Reeling=false;
+            }
+        }
     }
 }
