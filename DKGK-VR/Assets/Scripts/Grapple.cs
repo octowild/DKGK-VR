b72f555714d8ccd625e7375a5c914c226307d60d
octowild <139123949+octowild@users.noreply.github.com>
2025-02-14 14:08:02 +0530

    code work

--- DKGK-VR/Assets/Scripts/Grapple.cs
+++ DKGK-VR/Assets/Scripts/Grapple.cs
@@ -5,19 +5,38 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Grapple : MonoBehaviour
 {
-    
+    public GameObject GrappleGun;
+    public GameObject GrappleHead;
 
+    public float _GrappleHeadSpeed = 10f;
+
+    private Vector3 _GunDir;
+    private bool _Shooting=false;
 
     void Start()
     {
         XRGrabInteractable _grabinteractable = GetComponent<XRGrabInteractable>();
-        _grabinteractable.activated.AddListener();
-        _grabinteractable.deactivated.AddListener();
+        _grabinteractable.activated.AddListener(x=>GrappleShoot());
+        _grabinteractable.deactivated.AddListener(x=>GrappleStop());
     }
 
   
     void Update()
     {
-        
+        if (!_Shooting)
+        {
+            _GunDir = (GrappleGun.transform.up * -1);
+        }
+        if (_Shooting) {
+            GrappleHead.transform.position += _GunDir * _GrappleHeadSpeed * Time.deltaTime;
+        }
+    }
+    public void GrappleShoot()
+    {
+        _Shooting = true;
+    }
+    public void GrappleStop()
+    {
+        _Shooting = false;
     }
 }
