11a8d9611a653069e6711002085a67051d1cecb0
octowild <139123949+octowild@users.noreply.github.com>
2025-02-14 15:20:50 +0530

    Update Grapple.cs

--- DKGK-VR/Assets/Scripts/Grapple.cs
+++ DKGK-VR/Assets/Scripts/Grapple.cs
@@ -15,8 +15,6 @@ public class Grapple : MonoBehaviour
     private Vector3 _ReelDir;
     private bool _Shooting=false;
     private bool _HeadCol=false;
-    private bool _Attached = false;
-    private bool _release=false;
     private bool _Reeling=false;
 
     void Start()
@@ -26,7 +24,8 @@ public class Grapple : MonoBehaviour
         _grabinteractable.deactivated.AddListener(x=>GrappleStop());
     }
 
-  
+  // add button for reeling
+  //move player
     void Update()
     {
         if (!_Shooting)
@@ -37,11 +36,7 @@ public class Grapple : MonoBehaviour
         {
             GrappleHead.transform.position += _GunDir * _GrappleHeadSpeed * Time.deltaTime;
         }
-        else if (_Shooting && _HeadCol)
-        {
-            _Attached = true;
-        }
-        if (_Reeling) {
+        if (_Reeling||!_Shooting) {
             GrappleReel();
         }
     }
@@ -52,31 +47,26 @@ public class Grapple : MonoBehaviour
     public void GrappleStop()
     {
         _Shooting = false;
-        _release = true;
         //GrappleHead.transform.position=BarrelPos.transform.position;
     }
     public void GrappleReel()
     {
-        if (!_HeadCol)
+        if (!_HeadCol||!_Shooting)
         {
-            if (_Attached||_release)
-            {
                 _ReelDir = BarrelPos.transform.position- GrappleHead.transform.position;
                 GrappleHead.transform.position += _ReelDir.normalized * _GrappleHeadSpeed * Time.deltaTime;
-            }
-            else
-            {
-                _ReelDir = GrappleHead.transform.position- BarrelPos.transform.position;
-                //move player here
-            }
-
-
-
-            if (_ReelDir.magnitude <= 5f)
-            {
-                GrappleHead.transform.position=BarrelPos.transform.position;
-                _Reeling=false;
-            }
         }
+        else if (_HeadCol) 
+        {
+            _ReelDir = GrappleHead.transform.position- BarrelPos.transform.position;
+            //move player here
+        }
+
+        if (_ReelDir.magnitude <= 5f)
+        {
+            GrappleHead.transform.position = BarrelPos.transform.position;
+            _Reeling = false;
+        }
+        
     }
 }
