8f7bab458891dbae987385d8edb0c1a544b84260
octowild <139123949+octowild@users.noreply.github.com>
2025-02-20 02:38:04 +0530

    time to sleep

---
+++ DKGK-VR/Assets/Scripts/Rope.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+
+    public GameObject _GrappleGun;
+    public GameObject _Hook;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
